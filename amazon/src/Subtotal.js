8073044fb1d163bad798209fdcfd504bcd72ea58
Murad Cholukov <muradusa@gmail.com>
2020-09-15 19:20:09 -0700

    finishing up with checkout page

--- amazon/src/Subtotal.js
+++ amazon/src/Subtotal.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 //Delete later
 
@@ -13,7 +14,7 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              Subtotal ({basket.length}) items: <strong>250</strong>
+              Subtotal ({basket.length} items) : <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -21,7 +22,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
