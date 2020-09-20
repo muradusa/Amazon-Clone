7cf7c0187270126504da6d0117af474a8ef89421
Murad Cholukov <muradusa@gmail.com>
2020-09-19 19:12:06 -0700

    Update index.js

--- amazon/functions/index.js
+++ amazon/functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51HSZbFJjij9IehHufRnzScqXPH1pnkQi5wyPYixR9qQTm6d8rClU5ln0B8qSAjPgkodzYoWa1H079F3FW7IT9z4b00ZPeE3Kp2"
+  "SECRET KEY GOES HERE"
 );
 
 // App config
